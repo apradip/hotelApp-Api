503ae37384ee758c8a6b577606f6ed7ea1078674
apradip <pradip.adhikary@gmail.com>
2023-02-13 15:37:10 +0530

    latest

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:8000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    process.env.API_SERVER_IP.concat(":", process.env.API_SERVER_PORT),
+    process.env.FRONTEND_SERVER_IP.concat(":", process.env.FRONTEND_SERVER_PORT)
 ];
 
 module.exports = allowedOrigins;
