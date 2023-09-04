dc645e9ac0929ff3aa13adcd5f479793b2bc7766
apradip <pradip.adhikary@gmail.com>
2023-09-04 21:37:55 +0530

    update

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,10 +1,10 @@
 const allowedOrigins = [
-    process.env.API_DOMAIN,
+    // process.env.API_DOMAIN,
     process.env.FRONTEND_DOMAIN,
-    process.env.API_SERVER_IP,
+    // process.env.API_SERVER_IP,
     process.env.FRONTEND_SERVER_IP,
-    process.env.API_SERVER_IP.concat(":", process.env.API_HTTPS_SERVER_PORT),
-    process.env.API_SERVER_IP.concat(":", process.env.SOCKET_PORT),
+    // process.env.API_SERVER_IP.concat(":", process.env.API_HTTPS_SERVER_PORT),
+    // process.env.API_SERVER_IP.concat(":", process.env.SOCKET_PORT),
     process.env.FRONTEND_SERVER_IP.concat(":", process.env.FRONTEND_SERVER_PORT),
     process.env.FRONTEND_SERVER_IP.concat(":", process.env.SOCKET_PORT)
 ];
