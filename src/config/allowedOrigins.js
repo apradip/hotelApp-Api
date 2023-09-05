0061b8ba25dbc05c18b81eab4d3877bdcfce2d8e
apradip <pradip.adhikary@gmail.com>
2023-09-05 09:48:15 +0530

    update

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,12 +1,11 @@
 const allowedOrigins = [
-    // process.env.API_DOMAIN,
     process.env.FRONTEND_DOMAIN,
-    // process.env.API_SERVER_IP,
     process.env.FRONTEND_SERVER_IP,
-    // process.env.API_SERVER_IP.concat(":", process.env.API_HTTPS_SERVER_PORT),
-    // process.env.API_SERVER_IP.concat(":", process.env.SOCKET_PORT),
     process.env.FRONTEND_SERVER_IP.concat(":", process.env.FRONTEND_SERVER_PORT),
-    process.env.FRONTEND_SERVER_IP.concat(":", process.env.SOCKET_PORT)
+    process.env.FRONTEND_SERVER_IP.concat(":", process.env.SOCKET_PORT),
+    process.env.GOOGLE_DOMAIN,
+    process.env.GOOGLE_CLOUD_DOMAIN,
+    process.env.GOOGLE_DIALOGFLOW_DOMAIN,
 ];
 
 module.exports = allowedOrigins;
