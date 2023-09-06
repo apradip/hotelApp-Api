7b1e666addf91ae15103932bbb98edbd9faaf420
apradip <pradip.adhikary@gmail.com>
2023-09-06 17:16:23 +0530

    update

--- src/controllers/dialogFlow/df_rooms.js
+++ src/controllers/dialogFlow/df_rooms.js
@@ -18,49 +18,54 @@ const handelPlaceList = async (agent) => {
 };
 
 const handelRoomEnquiry = async (agent) => {
-    const place = agent.context.get("roomenquiryintent-followup").parameters["place"];
-    const start_date = agent.context.get("roomenquiryintent-followup").parameters["startdate"];
-    const end_date = agent.context.get("roomenquiryintent-followup").parameters["enddate"];
-    const no_of_border = agent.context.get("roomenquiryintent-followup").parameters["noofboder"];
+    const city = agent.context.get("GetPlaceIntent").parameters["PlaceEntity"];
+    const start_date = agent.context.get("GetStartDateIntent").parameters["StartDate"];
+    const no_of_days = agent.context.get("GetNoOfDayToStayIntent").parameters["NoOfDayToStay"];
+    const no_of_borders = agent.context.get("GetBorderCountIntent").parameters["BorderCount"];
 
     // console.log(start_date);
     // console.log(no_of_days_to_stay);
     
-    const data = await Room.find({hotelId, isEnable: true, isOccupied: false});
-    let isAvailable = false;
+    const dataHotel = await Hotel.find({city: city, isEnable: true}).select({_id: 1});
+    
+    if (dataHotel.length > 0) {
+      const dataRoom = await Room.find({hotelId: dataHotel._id, isEnable: true, isOccupied: false});
+      
+      let isAvailable = false;
 
-    if (data) {
-        if (data.length) {
-            isAvailable = true;
-        }
+      if (dataRoom) {
+          if (dataRoom.length) {
+              isAvailable = true;
+          }
+      }
     }
 
     if (isAvailable) {
-      const response = 
-      {
-        "richContent": [
-          [
-            {
-              "type": "chips",
-              "options": [
-                {
-                  "mode": "blocking",
-                  "text": "Chip 1"
-                },
-                {
-                  "text": "Chip 2"
-                }
-              ]
-            }
-          ]
-        ]
-      };
+      // const response = 
+      // {
+      //   "richContent": [
+      //     [
+      //       {
+      //         "type": "chips",
+      //         "options": [
+      //           {
+      //             "mode": "blocking",
+      //             "text": "Chip 1"
+      //           },
+      //           {
+      //             "text": "Chip 2"
+      //           }
+      //         ]
+      //       }
+      //     ]
+      //   ]
+      // };
 
       // agent.add(new dfff.Payload(agent.UNSPECIFIED, response, { rawPayload: true, sendAsMessage: true}));
 
-      agent.add(`Hello, great news we have ${data.length} rooms avaliable`);    
+      agent.add(`GERAT! we have room availability. Prices are as follows per day +GST`);    
     } else {
-      agent.add(`Sorry, we have no room on that time`);    
+      agent.add(`SORRY, we have dosen't have any room avaliable.`);    
     }
 };
 
